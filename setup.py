7760ccbcf20a85f7eef9d1888978c0da31c8354e
manti <manti@practo.com>
2017-02-09 03:03:56 +0530

    adding setup file for to register application

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='pincode-map',
+      version='1.0',
+      description='Validate and Mapping Pincode to Locality, City and State ',
+      url='https://github.com/manti/pincode-map',
+      author='Manti HS',
+      author_email='manti.rvce@gmail.com',
+      license='MIT',
+      zip_safe=False,
+      packages=['pincodemap'],
+      package_dir={'pincode-map': 'pincode-map/'},
+    )
